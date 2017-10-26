aa9901f94e61082cb69b84245fb345894406aaad
Incraigulous <me@craigwann.com>
2017-10-26 16:34:13 -0500

    Autoinstalling flipster.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import CoverFlow from './CoverFlow'
 import CoverFlowSlide from './CoverFlowSlide'
 
+if (typeof $.flipster == "undefined") { 
+  require('jquery.flipster')
+}
+
 export default CoverFlow
 
 export {CoverFlow, CoverFlowSlide}
